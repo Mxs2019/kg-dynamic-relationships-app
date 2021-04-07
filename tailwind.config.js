c42e59336965474171b8826335f844446e14cf57
Max Shaw <maxdshaw@gmail.com>
2021-04-07 16:00:36 -0700

    switch to jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  purge: [],
+  purge: [
+    "./components/**/*.tsx",
+    "./src/**/*.tsx",
+    "./src/**/*.js",
+    "./src/**/*.jsx",
+    "./pages/**/*.tsx",
+    "./pages/**/*.js",
+  ],
+  mode: "jit",
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
